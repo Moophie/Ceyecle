af51240999eb4981b25d08093700f6882c3dd2ec
Moophie <michael.van.lierde@hotmail.com>
2021-03-24 15:45:19 +0100

    Basic navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/social"><img src="https://via.placeholder.com/50" alt="Social icon"></a>
+    <a href="/"><img src="https://via.placeholder.com/50" alt="Home icon"></a>
+    <a href="/profile"><img src="https://via.placeholder.com/50" alt="Profile icon"></a>
+</nav>
