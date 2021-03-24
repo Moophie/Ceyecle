f17b000152c58d5786053388f602ec6cdf0ca9c8
Moophie <michael.van.lierde@hotmail.com>
2021-03-24 15:36:56 +0100

    Basic index structure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,21 @@
 
 @section('content')
 
-<h1>Hello</h1>
+<img src="https://via.placeholder.com/50" alt="Ceyecle small logo">
+<div class="current-races"><p>ETHIAS CROSS EEKLO 2021</p><img src="" alt=""></div><img src="https://via.placeholder.com/50" alt="Profile icon">
+
+<h1>Upcoming events</h1>
+
+<!-- Event template, make dynamic -->
+<div class="race-event">
+    <img src="https://via.placeholder.com/100" alt="Event image">
+    <h2>Kuurne-Brussel-Kuurne</h2>
+    <p>28/02/2021</p>
+    <a>Meer info</a>
+</div>
+
+@component('components/navbar')
+
+@endcomponent
 
 @endsection
