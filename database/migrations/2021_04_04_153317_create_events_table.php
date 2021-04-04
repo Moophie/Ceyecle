47e98ba10bd07c5ce4123fc8e77b0a6008d48890
Moophie <michael.van.lierde@hotmail.com>
2021-04-04 17:37:23 +0200

    perf: add migrations and models

---
+++ database/migrations/2021_04_04_153317_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->dateTime('time');
+            $table->string('thumbnail');
+            $table->foreign('route_id')->references('id')->on('cycle_routes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
