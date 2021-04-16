2c42ac76a780a262a3450db7b6666f231c743da0
Moophie <michael.van.lierde@hotmail.com>
2021-04-16 18:48:31 +0200

    dev: make seeder for events from api

--- app/Classes/SportsradarAPI.php
+++ app/Classes/SportsradarAPI.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Http;
 
 class SportsradarAPI
 {
-
-    public function getSeasonEvents()
+    public static function getSeasonEvents()
     {
         $base_url = 'https://api.sportradar.us/cycling/trial/v2/nl/';
 
@@ -17,15 +16,17 @@ class SportsradarAPI
         $currentSeasonId = $response['stages'][0]['id'];
 
         // Then get all the events
-        $response = Http::get($base_url . 'sport_events/' . $currentSeasonId . '/schedule.json?api_key=' . env('SPORTSRADAR_API_KEY'));
+        // Wait a second before doing the next call
+        sleep(1);
+        $response = Http::get($base_url . 'sport_events/' . $currentSeasonId . '/schedule.json?api_key=' . env('SPORTSRADAR_API_KEY'))->json();
         return $response['stages'];
     }
 
-    public function getCyclerInfo(){
-
+    public function getCyclerInfo()
+    {
     }
 
-    public function getEventInfo(){
-
+    public function getEventInfo()
+    {
     }
 }
