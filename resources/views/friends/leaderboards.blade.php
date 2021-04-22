b587c7d6be4330b6a5f07d48409af287ffff0d23
Hannah De Wachter <dewachterhannah@gmail.com>
2021-04-22 17:48:02 +0200

    leaderboard

--- resources/views/friends/leaderboards.blade.php
+++ resources/views/friends/leaderboards.blade.php
@@ -6,11 +6,21 @@
 
 @section('content')
 
-    @foreach($friends as $friend)
+    @php
+        $user = Auth::user();
+        array_push($friends, $user);
+        usort($friends, function($a, $b) {
+                        return $a->score < $b->score ? 1 : -1;
+                    });
+    @endphp
+
+    @for($i = 0; $i < count($friends); $i++)
         <div>
-            <h2>{{ $friend->username }}</h2>
+            <span>{{ $i+1 }}</span>
+            <p>{{ $friends[$i]['username'] }}</p>
+            <p>{{ $friends[$i]['score'] }}</p>
         </div>
-    @endforeach
+    @endfor
 
 @component('components/navbar')
 @endcomponent
