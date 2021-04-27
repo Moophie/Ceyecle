8a98a55a7d0e422491bca4b4ce81beebba23b6d1
Moophie <michael.van.lierde@hotmail.com>
2021-04-27 15:51:24 +0200

    feat: change layout and add example pic of stage_profile

--- resources/views/rooms/show.blade.php
+++ resources/views/rooms/show.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts/app')
 
 @section('content')
-<h1>{{ $room->event->name }}</h1>
-<form action="/rooms/invite" method="POST">
+    <h1>{{ $room->event->name }}</h1>
+    <img src="/images/stage_profiles/example.jpg" alt="">
 
-    {{ csrf_field() }}
-
-    <input type="text" value="{{ $room->id }}" name="room-id" hidden>
-    <input type="submit" value="Invite friends">
-    <h1>Friends in room</h1>
+    <h1>Viewers</h1>
     @foreach ($room->users as $user)
-    {{ $user->username }}
-
+        <p>{{ $user->username }}</p>
     @endforeach
-</form>
+    <form action="/rooms/invite" method="POST">
+
+        {{ csrf_field() }}
+
+        <input type="text" value="{{ $room->id }}" name="room-id" hidden>
+        <input type="submit" value="Invite friends">
+    </form>
 @endsection
