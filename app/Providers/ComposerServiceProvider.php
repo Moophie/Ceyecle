b42c338d05b897f7c604962e152dfd13e456316e
Hannah De Wachter <dewachterhannah@gmail.com>
2021-05-20 12:13:18 +0200

    friend request notification in navbar

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -24,6 +24,6 @@ class ComposerServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer(['friends/list', 'friends/leaderboards', 'rooms/invite'], \App\Http\ViewComposers\FriendsComposer::class);
-        view()->composer(['friends/list', 'friends/index'], \App\Http\ViewComposers\RequestComposer::class);
+        view()->composer(['friends/list', 'friends/index', 'components/navbar'], \App\Http\ViewComposers\RequestComposer::class);
     }
 }
