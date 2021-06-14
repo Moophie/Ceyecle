0ac40be43bc705ff765c6422568b3b47f87a4dc2
Moophie <michael.van.lierde@hotmail.com>
2021-06-14 16:45:40 +0200

    feat: add firebase tutorial code

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\WebNotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -87,3 +88,7 @@ Route::post('/rooms/answerQuestion', 'App\Http\Controllers\QuestionController@an
 
 // Races
 Route::get('/races/{race}', 'App\Http\Controllers\RaceController@showRace');
+
+Route::get('/push-notificaiton', [WebNotificationController::class, 'index'])->name('push-notificaiton');
+Route::post('/store-token', [WebNotificationController::class, 'storeToken'])->name('store.token');
+Route::post('/send-web-notification', [WebNotificationController::class, 'sendWebNotification'])->name('send.web-notification');
