fc31fee507c3fd011d022a878fe627353d5f0b90
lizevandenberg <43468244+lizevandenberg@users.noreply.github.com>
2021-06-14 15:15:43 +0200

    css

--- resources/views/friends/chatOverview.blade.php
+++ resources/views/friends/chatOverview.blade.php
@@ -9,7 +9,7 @@
     <h1>Chats</h1>
 
     @foreach($friendships as $friendship)
-        <a href="/friends/chat/{{ $friendship['other_user']->id }}">
+        <a href="./friends/chat/{{ $friendship['other_user']->id }}">
             <img src="{{ $friendship['other_user']->profilepic }}" alt="">
             <h2>{{ $friendship['other_user']->username }}</h2>
             <p>{{ $friendship['latest_chat']->content }}</p>
--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
 <div class="container">
-    <a class="knop" id="knopone" href="/friends/search" class="btn">Zoek Vrienden</a>
-    <a class="knop" id="knopone" href="/friends/list" class="btn">Bekijk vriendenlijst</a> @if($requests)<span style="background-color: #3C7A84; padding: 2px 5px; border-radius: 5px; color: white;">{{ count($requests) }}</span>@endif
-    <a class="knop" id="knopone" href="/leaderboard" class="btn">Bekijk scorebord</a>
-    <a class="knop" id="knopone" href="/chat" class="btn">Chat</a>
+    <a class="knop" id="knopone" href="./friends/search" class="btn">Zoek Vrienden</a>
+    <a class="knop" id="knopone" href="./friends/list" class="btn">Bekijk vriendenlijst</a> @if($requests)<span style="background-color: #3C7A84; padding: 2px 5px; border-radius: 5px; color: white;">{{ count($requests) }}</span>@endif
+    <a class="knop" id="knopone" href="./leaderboard" class="btn">Bekijk scorebord</a>
+    <a class="knop" id="knopone" href="./chat" class="btn">Chat</a>
 </div>
 
 @component('components/navbar')
