699f3e5798f3ee8f594314c828193f523e50ae00
Hannah De Wachter <dewachterhannah@gmail.com>
2021-06-14 16:13:58 +0200

    fix: when no chats

--- resources/views/friends/chatOverview.blade.php
+++ resources/views/friends/chatOverview.blade.php
@@ -9,12 +9,21 @@
     <h1>Chats</h1>
 
     @foreach($friendships as $friendship)
-        <a href="./friends/chat/{{ $friendship['other_user']->id }}">
-            <img src="{{ $friendship['other_user']->profilepic }}" alt="">
-            <h2>{{ $friendship['other_user']->username }}</h2>
-            <p>{{ $friendship['latest_chat']->content }}</p>
-            @if($friendship['latest_chat']->status == 'unread')<span style="background-color: #3C7A84; padding: 2px 5px; border-radius: 5px; color: white;">nieuw</span>@endif
-        </a>
+        @if($friendship['latest_chat'])
+            <a href="./friends/chat/{{ $friendship['other_user']->id }}">
+                <img src="{{ $friendship['other_user']->profilepic }}" alt="">
+                <h2>{{ $friendship['other_user']->username }}</h2>
+                <p>{{ $friendship['latest_chat']->content }}</p>
+                @if($friendship['latest_chat']->status == 'unread')<span style="background-color: #3C7A84; padding: 2px 5px; border-radius: 5px; color: white;">nieuw</span>@endif
+            </a>
+        @else
+            <p>Geen chats beschikbaar</p>
+            <h2>Vrienden om mee te chatten</h2>
+            <div>
+                <h3>{{ $friendship['other_user']->username }}</h3>
+                <a class="knop" href="./chat/{{ $friendship['other_user']->id }}">Chat</a>
+            </div>
+        @endif
     @endforeach
 
 @component('components/navbar')
