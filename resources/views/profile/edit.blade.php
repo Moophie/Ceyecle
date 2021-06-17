8db951c3877acf72fc6703a4255475fce6fc1b5f
Hannah De Wachter <dewachterhannah@gmail.com>
2021-06-17 16:37:45 +0200

    remove deelgenomen aan kader

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -25,14 +25,6 @@
         </form>
     </div>
 
-    <div class="kader">
-        <h2>Deelgenomen aan</h2>
-        <img src="https://via.placeholder.com/100" alt="Race picture">
-        <h2>WK Veldrijden 2021</h2>
-        <p class="me">30-31/01/2021</p>
-        <a href="">Meer info</a>
-    </div>
-
     @component('components/navbar')
     @endcomponent
 
